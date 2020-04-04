1a4e35b17203beb2d9581e6d605e0d5e4568a35c
rayn-victor <43250423+rayn-victor@users.noreply.github.com>
2020-04-04 14:09:16 -0400

    rayn.appbuild

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 (() => {
-    let upButton        = document.querySelector("#icon-up");
-        downButton      = document.querySelector("#icon-down");
-        mainNav         = document.querySelector(".mainNav");
+    let     upButton        = document.querySelector("#icon-up"),
+            downButton      = document.querySelector("#icon-down"),
+            mainNav         = document.querySelector(".mainNav");
+
+    var     words           = document.querySelector(".words");
+
 
         function scrollToTop(e) {
             e.preventDefault();
-            TweenLite.to(window, 0.7, {scrollTo:{y:mainNav, offsetY: 20}, autoKill: false});
+            TweenLite.to(window, 0.7, {scrollTo:{y:mainNav}, offsetY:20, autoKill: false});
         }
 
         function scrollToNext(e) {
             e.preventDefault();
-            TweenLite.to(window, 1.7, {scrollTo:{y:mainNav, offsetY: 20}, autoKill: false});
+            TweenLite.to(window, 1.7, {scrollTo:{y:mainNav}, offsetY:20, autoKill: false});
         }
 
-        // hero fade to black
+        // hero fade to black (credit css tricks)
         $(document).ready(function() {
             $(window).scroll(function(event) {
                 let scroll = $(this).scrollTop();
@@ -23,6 +26,15 @@
                 }
             });
         });
+
+        // mouse movement (credit jsfiddle)
+        $(window).mousemove(function(event) {
+            $(".words").css({
+              "margin-left": -(event.pageX * 0.02),
+              "margin-top": -(event.pageY * 0.02)
+            });
+          });
+          
 
         upButton.addEventListener('click', scrollToTop);
         downButton.addEventListener('click', scrollToNext);
